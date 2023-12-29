d3a5843bd6278ca66b9a8a414598931b3d832016
Leo <50770485+leoleoleo77@users.noreply.github.com>
2023-12-29 14:28:03 +0200

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import csv
-
-longitudeData = []
-with open('E:/Uni/SEM5/anag prwtipwn/housing.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=',')
-    print(list(spamreader)[0][0])
